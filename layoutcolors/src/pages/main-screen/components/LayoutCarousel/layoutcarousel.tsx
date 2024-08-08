a902f54dee6a7b4df4b7b3bb5ef163dd987a6c45
Santiago Oliveira <santiagosof@gmail.com>
2024-08-07 22:24:46 -0300

    +

--- layoutcolors/src/pages/main-screen/components/LayoutCarousel/layoutcarousel.tsx
+++ layoutcolors/src/pages/main-screen/components/LayoutCarousel/layoutcarousel.tsx
@@ -3,6 +3,8 @@ import Layout1 from '../Models/Layout1/layout1';
 import Layout2 from '../Models/Layout2/layout2';
 import Layout3 from '../Models/Layout3/layout3';
 import "./layoutcarousel.css"
+import setaesquerda from "./images/setaleft.png"
+import setadireita from "./images/setaright.png"
 
 const layouts = [<Layout1 />, <Layout2 />, <Layout3 />];
 
@@ -18,12 +20,13 @@ const LayoutCarousel = () => {
   };
 
   return (
-    <div style={{ width: '900px', height: '480px', border: '1px solid #ccc', position: 'relative' }}>
-      <button onClick={handlePrev} style={{ position: 'absolute', top: '50%', left: '0px', transform: 'translateY(-50%)' }}><img src="../../images/setaleft" alt="" /></button>
+    <div style={{ width: '1020px', height: '480px', border: '1px', position: 'absolute' }}>
+      <button onClick={handlePrev} style={{ position: 'absolute', top: '50%', left: '-0px', transform: 'translateY(-50%)' }}> 
+      <img src={setaesquerda} alt="" /></button>
       <div style={{ width: '100%', height: '100%' }}>
         {layouts[currentLayout]}
       </div>
-      <button className='buttonleftright' onClick={handleNext} style={{ position: 'absolute', top: '50%', right: '0', transform: 'translateY(-50%)' }}>▶</button>
+      <button onClick={handleNext} style={{ position: 'absolute', top: '50%', right: '0', transform: 'translateY(-50%)' }}><img src={setadireita} alt="" /></button>
     </div>
   );
 };
--- layoutcolors/src/pages/main-screen/components/Models/Layout1/layout1.tsx
+++ layoutcolors/src/pages/main-screen/components/Models/Layout1/layout1.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import "../../Models/geral.css";
 import "./layout1.css"
 
 const Layout1 = () => {
-
   return (
-    <div className="layout1">
+    <div className="layout" id="layout1">
       <h1>Layout 1</h1>
       <p>Este é o primeiro layout.</p>
     </div>
