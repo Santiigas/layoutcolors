bf3bbaeed932571b9bcccba09b5c85717b60c532
Santiago Oliveira <santiagosof@gmail.com>
2024-08-16 21:40:47 -0300

    +

--- layoutcolors/src/pages/main-screen/components/Models/Layout3/layout3.tsx
+++ layoutcolors/src/pages/main-screen/components/Models/Layout3/layout3.tsx
@@ -5,6 +5,22 @@ import "./layout3.css";
 const Layout3 = () => {
   return (
     <div className="layout" id="layout3">
+      <header className="header_top_layout3"></header>
+      <header className="header_center_layout3">
+        <div className="logo_layout3"></div>
+        <div className="search_bar_layout3"></div>
+        <div className="box_layout3"></div>
+        <div className="box_layout3"></div>
+      </header>
+      <header className="header_botton_layout3">
+        <div className="option_layout3">Opção</div>
+        <div className="option_layout3">Opção</div>
+        <div className="option_layout3">Opção</div>
+        <div className="option_layout3">Opção</div>
+        <div className="option_layout3">Opção</div>
+        <div className="option_layout3">Opção</div>
+        <div className="option_layout3">Opção</div>
+      </header>
     </div>
   );
 };
