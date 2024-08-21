a2ff80f950791b1334113ee7b0f61a57d1503fc8
Santiago Oliveira <santiagosof@gmail.com>
2024-08-20 22:37:37 -0300

    +

--- layoutcolors/src/pages/main-screen/components/Models/Layout4/layout4.tsx
+++ layoutcolors/src/pages/main-screen/components/Models/Layout4/layout4.tsx
@@ -1,19 +1,77 @@
 import React from "react";
 import "../../Models/geral.css";
 import "./layout4.css";
+import estrela_preenchida from "./estrela_preenchida.png"
+import ps5 from "./ps5.jpg"
+
 
 const Layout4 = () => {
   return (
     <div className="layout" id="layout4">
       <div className="buy_products_layout4">
         <div className="product_layout4">
-          <div className="image_layout4"></div>
+          <div className="config_image_layout4">
+            <img className="image_pub_ps5" src={ps5} alt="" />
+            <div className="image_layout4"></div>
+            <div className="bar_layout4"></div>
+          </div>
           <div className="information_layout4">
             <h1 className="h1_layout4">Console PlayStation®5 + Marvel's Spider-Man 2</h1>
-            <h2 className="h2_layout4">R$4.749,00</h2>
+            <h2 className="h2_layout4">R$3.950,00</h2>
             <h3 className="h3_layout4">À vista no PIX com até 5% OFF <br/> 12x de R$ 102,93 sem juros</h3>
+            <div className="stars_layout4">
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+            </div>
           </div>
-          <div className="buy_bar_layout4">ADICIONAR AO CARRRINHO</div>
+          <div className="buy_bar_layout4">ADICIONAR AO CARRINHO</div>
+        </div>
+      </div>
+      <div className="buy_products_layout4">
+        <div className="product_layout4">
+          <div className="config_image_layout4">
+            <img className="image_pub_ps5" src={ps5} alt="" />
+            <div className="image_layout4"></div>
+            <div className="bar_layout4"></div>
+          </div>
+          <div className="information_layout4">
+            <h1 className="h1_layout4">Console PlayStation®5 + Marvel's Spider-Man 2</h1>
+            <h2 className="h2_layout4">R$3.950,00</h2>
+            <h3 className="h3_layout4">À vista no PIX com até 5% OFF <br/> 12x de R$ 102,93 sem juros</h3>
+            <div className="stars_layout4">
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+            </div>
+          </div>
+          <div className="buy_bar_layout4">ADICIONAR AO CARRINHO</div>
+        </div>
+      </div>
+      <div className="buy_products_layout4">
+        <div className="product_layout4">
+          <div className="config_image_layout4">
+            <img className="image_pub_ps5" src={ps5} alt="" />
+            <div className="image_layout4"></div>
+            <div className="bar_layout4"></div>
+          </div>
+          <div className="information_layout4">
+            <h1 className="h1_layout4">Console PlayStation®5 + Marvel's Spider-Man 2</h1>
+            <h2 className="h2_layout4">R$3.950,00</h2>
+            <h3 className="h3_layout4">À vista no PIX com até 5% OFF <br/> 12x de R$ 102,93 sem juros</h3>
+            <div className="stars_layout4">
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+              <img className="image2_layout4" src={estrela_preenchida} alt="" />
+            </div>
+          </div>
+          <div className="buy_bar_layout4">ADICIONAR AO CARRINHO</div>
         </div>
       </div>
     </div>
