de8ad5900648f55fcaab29b11284f5672bcbd68f
Santiago Oliveira <santiagosof@gmail.com>
2024-08-25 14:35:02 -0300

    +

--- layoutcolors/src/pages/main-screen/components/Models/Layout5/layout5.tsx
+++ layoutcolors/src/pages/main-screen/components/Models/Layout5/layout5.tsx
@@ -20,7 +20,25 @@ const Layout5 = () => {
             <div className="box_layout5"></div>
           </div>
         </div>
-        <div className="information_layout5"></div>
+        <div className="information_layout5">
+          <h3>Mostre seu amor e orgulho pelo Red Devils com a Camisa Manchester United Home</h3>
+          <h1>R$ 199,99</h1>
+          <h2>até 4x de R$ 50,00 sem juros</h2>
+          <h4>Cor: Vermelho Escuro</h4>
+          <h3>Escolha seu tamanho:</h3>
+          <div className="size_layout5">
+            <div className="size_options">P</div>
+            <div className="size_options">M</div>
+            <div className="size_options">G</div>
+            <div className="size_options">GG</div>
+          </div>
+          <div className="buy">
+            <div className="buy_layout5">COMPRAR</div>
+            <div className="shopping_cart">+CARRINHO</div>
+          </div>
+          <h3>Frete: 10,99 - Chega dia 4 de setembro ao escolher a opção de 
+            entrega Expressa para o CEP 00000-000.</h3>
+        </div>
       </div>
     </div>
   );
