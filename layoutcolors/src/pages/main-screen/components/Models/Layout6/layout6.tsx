cbb47240f47d892f7ef52f309d13a0d73a743962
Santiago Oliveira <santiagosof@gmail.com>
2024-08-26 21:59:49 -0300

    +

---
+++ layoutcolors/src/pages/main-screen/components/Models/Layout6/layout6.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../../Models/geral.css";
+import "./layout6.css";
+
+const Layout6 = () => {
+  return (
+    <div className="layout" id="layout6">
+      <div className="screen_layout6">
+        <div className="main_screnn">
+          <div className="main_bar">
+            <div className="box1_layout6"></div>
+            <div className="box2_layout6">
+              <div className="text">
+                <h1>NOVAS PROVAS DO ENEM DEVEM SER FEITAS</h1>
+              </div>
+              <h2>Será que alguém explica a nossa relação?
+                  Um caso indefinido, mas rola paixão
+                  Adoro esse perigo, mexe demais comigo</h2>  
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+  
+export default Layout6;
